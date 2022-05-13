8657bbd211351e543cb149e5d19fe73b4a03d9ec
Terry Yi Wang <terry@clubhouse.com>
2022-05-13 19:28:32 -0400

    Add cell

---
+++ RXSwiftDemo/View Controllers/UserDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  UserDetailViewController.swift
+//  RXSwiftDemo
+//
+//  Created by Terry Wang on 5/13/22.
+//
+
+import UIKit
+
+class UserDetailViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
---
+++ RXSwiftDemo/Views/UserCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserCell.swift
+//  RXSwiftDemo
+//
+//  Created by Terry Wang on 5/13/22.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
